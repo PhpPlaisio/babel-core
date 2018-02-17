7caa959b24cae2619a1c590b0b69d896f8b48b01
Paul Water <p.r.water@setbased.nl>
2018-02-17 16:27:30 +0100

    Reorganized unit tests.

--- test/C.php
+++ test/C.php
@@ -13,6 +13,21 @@ class C
    *
    * Below this doc block constants will be inserted by PhpStratum.
    */
+  const LAN_ID_EN                 = 1;
+  const LAN_ID_NL                 = 2;
+  const LAN_ID_RU                 = 3;
+  const TTG_ID_TEST               = 1;
+  const TXT_ID_FORMATTED1         = 3;
+  const TXT_ID_HELLO_TEXT         = 1;
+  const TXT_ID_HELLO_TEXT_SPECIAL = 2;
+  const TXT_ID_REPLACED1          = 4;
+  const WDG_ID_LANGUAGE           = 1;
+  const WDG_ID_TEST               = 2;
+  const WRD_ID_DUTCH              = 2;
+  const WRD_ID_ENGLISH            = 1;
+  const WRD_ID_HELLO_WORD         = 4;
+  const WRD_ID_HELLO_WORD_SPECIAL = 5;
+  const WRD_ID_RUSSIAN            = 3;
 
 
   //--------------------------------------------------------------------------------------------------------------------
